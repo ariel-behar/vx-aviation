1db9b9b205ab92e7ce3b40bd89f00f159bf8bb88
Ariel Behar <ariel.behar@hotmail.com>
2024-02-12 20:21:37 +0200

    Creating and implementing the BackgroundVideo component

---
+++ src/components/ViewSpecificComponents/BackgroundVideo.tsx
@@ -0,0 +1,35 @@
+import { useRef } from 'react';
+
+import flightSchoolVideo from '../../assets/video/flight-school-video.mp4';
+import flightSchoolPoster from '../../assets/video/flight-school-poster.jpg'
+
+function BackgroundVideo() {
+    const videoRef = useRef<null | HTMLVideoElement>(null);
+
+    const handlePlay = () => {
+        if (videoRef.current != null) {
+            videoRef.current.play();
+        }
+    };
+
+    return (
+        <>
+            <button className='hidden' onClick={handlePlay}>Play Video</button>
+
+            <video
+                poster={flightSchoolPoster}
+                ref={videoRef}
+                className='object-cover w-full h-full fixed top-0 left-0 z-0'
+                autoPlay
+                muted
+                loop
+                playsInline
+            >
+                <source src={flightSchoolVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+            </video>
+        </>
+    )
+}
+
+export default BackgroundVideo
