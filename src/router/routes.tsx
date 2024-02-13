b294f89b70b9b4632ea133fa90c52af56b92af97
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 16:14:04 +0200

    Lazily loading remaining views from router

--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -5,6 +5,9 @@ const LazyRootView = lazy(() => import("../views/RootView"));
 const LazyErrorView = lazy(() => import("../views/ErrorView"));
 
 const LazyHomeView = lazy(() => import("../views/HomeView"));
+const LazyFlightSchoolView = lazy(() => import("../views/FlightSchoolView"));
+const LazyMaintenanceServicesView = lazy(() => import("../views/MaintenanceServicesView"));
+const LazyAirplanesForSaleView = lazy(() => import("../views/AirplanesForSaleView"));
 
 const router = createBrowserRouter([
     {
@@ -15,6 +18,18 @@ const router = createBrowserRouter([
             {
                 path: "/",
                 element: <LazyHomeView />
+            },
+            {
+                path: '/flight-school',
+                element: <LazyFlightSchoolView />
+            },
+            {
+                path: '/services',
+                element: <LazyMaintenanceServicesView />
+            },
+            {
+                path: '/airplanes-for-sale',
+                element: <LazyAirplanesForSaleView />
             }
         ]
     }
