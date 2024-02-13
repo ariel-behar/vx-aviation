0aef0982bf7d3a2f338f54d11bf6b9ac7d00050a
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 16:36:17 +0200

    Implementing the Loader in the Suspense component

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import './App.scss'
 
 import router from './router/routes'
 import { Suspense } from 'react'
+import Loader from './components/Loader'
 
 function App() {
 
 	return (
 		<>
 			<ThemeProvider>
-				<Suspense fallback={<div>Loading...</div>}>
+				<Suspense fallback={<Loader />}>
 					<RouterProvider router={router} />
 				</Suspense>
 			</ThemeProvider>
