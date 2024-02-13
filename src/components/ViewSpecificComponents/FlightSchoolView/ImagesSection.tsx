5cacdb60e261365a560ac7e75f80ad7a26d9d603
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 20:35:48 +0200

    Creating the ImagesSection and adding it to the FlightSchoolView

---
+++ src/components/ViewSpecificComponents/FlightSchoolView/ImagesSection.tsx
@@ -0,0 +1,40 @@
+import airplaneFlying from '../../../assets/img/airplane-flying.jpg'
+import cockpit from '../../../assets/img/cockpit.jpg'
+import pilotEpaulettes from '../../../assets/img/pilot-epaulettes.jpg'
+
+interface Image {
+    imgSrc: string
+    alt: string
+}
+
+const images: Image[] = [
+    {
+        imgSrc: pilotEpaulettes,
+        alt: "Pilot Epaulettes"
+    },
+    {
+        imgSrc: cockpit,
+        alt: "Cockpit"
+    },
+    {
+        imgSrc: airplaneFlying,
+        alt: "Airplane flying"
+    }
+]
+
+function ImagesSection() {
+    return (
+        <section className="grid grid-cols-3">
+            {
+                images.map(({ imgSrc, alt }: Image) => (
+                    <div className='relative'>
+                        <img src={imgSrc} alt={alt} />
+                        <div className='absolute top-0 left-0 w-full h-full bg-black/30' />
+                    </div>
+                ))
+            }
+        </section>
+    )
+}
+
+export default ImagesSection
--- src/views/FlightSchoolView.tsx
+++ src/views/FlightSchoolView.tsx
@@ -1,9 +1,12 @@
 import HeroSection from "../components/ViewSpecificComponents/FlightSchoolView/HeroSection"
+import ImagesSection from "../components/ViewSpecificComponents/FlightSchoolView/ImagesSection"
+import WhoWeAreSection from "../components/ViewSpecificComponents/FlightSchoolView/WhoWeAreSection"
 
 function FlightSchoolView() {
 	return (
 		<>
 			<HeroSection />
+			<ImagesSection />
 		</>
 	)
 }
