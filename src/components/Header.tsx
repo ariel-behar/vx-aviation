cc92444264b1e1287ad94cdee266766d5b1ec803
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 18:26:27 +0200

    Implementing styling for the Header's active link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import uniqid from "uniqid";
 
 import vxAviationLogo from '../assets/img/logos/vx-aviation-logo-small.png'
 
 import IconButton from "@material-tailwind/react/components/IconButton";
-import Navbar, { MobileNav} from "@material-tailwind/react/components/Navbar";
+import Navbar from "@material-tailwind/react/components/Navbar";
+import Collapse from "@material-tailwind/react/components/Collapse";
 
 interface Route {
     path: string;
@@ -51,11 +53,17 @@ export function Header() {
                 routes.map(({ path, title }: Route, index) => {
                     if (index !== 0) {
                         return (
-                            <li style={{textShadow: "3px 3px 2px rgba(15,15,15,0.8)"}} className=" blue-gray p-1 font-normal text-primary active:text-white" >
-                                <Link to={path} className="flex items-center">
+                            <NavLink
+                                to={path}
+                                className={({ isActive }) => (isActive && path !== '/flight-school#contact' ? 'text-white' : 'text-primary')}
+                                key={uniqid()}
+                            >
+                                <li style={{ textShadow: "3px 3px 2px rgba(15,15,15,0.8)" }} className="p-1 font-normal" >
+
                                     {title}
-                                </Link>
-                            </li>
+
+                                </li>
+                            </NavLink >
                         )
                     }
                 })
@@ -69,15 +77,14 @@ export function Header() {
             <Navbar className="sticky border-none bg-opacity-100 top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 bg-tertiary ">
                 <div className="container mx-auto px-2">
                     <div className="flex items-center justify-between">
-                        <Link to="/">
+                        <NavLink to="/">
                             <img className="max-h-[50px] lg:max-h-[80px] w-auto" src={vxAviationLogo} alt="VX Aviation logo" />
-                        </Link>
+                        </NavLink >
                         <div className="flex items-center gap-4">
                             <div className="mr-4 hidden lg:block">
                                 <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
                                     {navList}
                                 </ul>
-
                             </div>
                             <IconButton
                                 variant="text"
@@ -118,10 +125,10 @@ export function Header() {
                             </IconButton>
                         </div>
                     </div>
-                    <MobileNav open={openNav}>
+                    <Collapse open={openNav}>
                         {navList}
 
-                    </MobileNav>
+                    </Collapse>
                 </div>
             </Navbar>
         </header>
