720d1d9cdedf0fa3d1073440c6703f483eea4b36
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 16:35:39 +0200

    Creating the Loader and implementing it into the App

---
+++ src/components/Loader.tsx
@@ -0,0 +1,15 @@
+
+function Loader() {
+    return (
+        <div className="w-screen h-screen bg-black flex justify-center items-center z-50">
+            <p>
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="144" height="48" viewBox="0 0 144 48">
+                    <path className="origin-bottom custom-take-off-animation" fill="#5B9144" d="M59.124,34L56,29h-4l2.947,11H89c1.657,0,3-1.343,3-3s-1.343-3-3-3H78.998L69,18h-4l4.287,16H59.124z" />
+                    <rect id="ground" x="52" y="44" fill="#004080" width="40" height="4" />
+                </svg>
+            </p>
+        </div>
+    )
+}
+
+export default Loader
