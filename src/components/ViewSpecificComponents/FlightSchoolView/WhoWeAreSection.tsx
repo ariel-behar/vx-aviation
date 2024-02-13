08c4c2c4c1897fde957b3e232dd1fe85f2f1ad2f
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 21:54:34 +0200

    Creating WhoWeAreSection and implementing it into the FlightSchoolView

---
+++ src/components/ViewSpecificComponents/FlightSchoolView/WhoWeAreSection.tsx
@@ -0,0 +1,46 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import propeller from '../../../assets/img/propeller.png'
+import uniqid from 'uniqid'
+
+type Ratings = string[]
+
+const ratingsList:Ratings = [
+    'Private Pilot',
+    'Commercial rating',
+    'Multi-Engine rating',
+    'Flight instructor for single engine, Multi-Engine and Instrument ratings',
+    'Airline Transport Pilot rating',
+    'Airframe & Power Plant (A&P)',
+    'Inspection Authorization rating (I/A)'
+]
+
+function WhoWeAreSection() {
+    return (
+        <section style={{ background: 'linear-gradient(45deg, #3a3c60 0%, #0b0e37 100%)' }}>
+            <div style={{ backgroundImage: `url(${propeller})` }} className='p-1 lg:p-4 flex justify-end bg-cover bg-center bg-no-repeat'>
+                <div className='lg:w-3/5 text-white py-5 px-5 lg:px-10'>
+                    <h3 className='text-center my-10'>WHO WE ARE</h3>
+
+                    <p className='text-justify'>We are what you would consider a “one-stop-shop” for all your aviation needs. Our owner is a licensed flight instructor with over 25 years of experience who got his experience by mowing lawns at 14 years old and paying his way forward. Please see the list of ratings currently held.</p>
+
+                    <ul className='my-10 pl-5 lg:pl-8'>
+                        {
+                            ratingsList.map(rating => (
+                                <li key={uniqid()} className='my-3'>
+                                    <FontAwesomeIcon icon="plane" color="#2E7D32" size="xl" className='-rotate-45 mr-3' />
+                                    {rating}
+                                </li>
+                            ))
+                        }
+                    </ul>
+
+                    <p className='text-center'>We welcome you to call anytime and look forward to meeting you</p>
+                </div>
+            </div>
+
+
+        </section>
+    )
+}
+
+export default WhoWeAreSection
--- src/views/FlightSchoolView.tsx
+++ src/views/FlightSchoolView.tsx
@@ -7,6 +7,7 @@ function FlightSchoolView() {
 		<>
 			<HeroSection />
 			<ImagesSection />
+			<WhoWeAreSection />
 		</>
 	)
 }
