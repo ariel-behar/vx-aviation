9aab37674cd9efabdb62b57d8e2f1fe7648f3ab1
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 19:32:26 +0200

    Updating HeroSection's position and button size

--- src/components/ViewSpecificComponents/FlightSchoolView/HeroSection.tsx
+++ src/components/ViewSpecificComponents/FlightSchoolView/HeroSection.tsx
@@ -6,15 +6,15 @@ import Button from '@material-tailwind/react/components/Button'
 
 function HeroSection() {
     return (
-        <section>
-            <img src={pilotGear} alt="Pilot gear" className="object-cover object-top w-full h-screen lg:h-[90vh] lg:mt-[10vh] absolute top-0 left-0 z-0" />
-            <div className='bg-black/50 absolute top-0 left-0 h-screen lg:h-[90vh] lg:mt-[10vh] w-full z-[1] flex justify-center items-center'>
+        <section className='relative'>
+            <img src={pilotGear} alt="Pilot gear" className="object-cover object-top w-full h-screen top-0 left-0 z-0" />
+            <div className='bg-black/50 absolute  top-0 left-0 h-screen w-full z-[1] flex justify-center items-center'>
                 <div className='text-center'>
                     <h1 className='text-white text-3xl lg:text-7xl'>WELCOME TO V<sub>x</sub> AVIATION</h1>
                     <h2 className='text-white text-xl lg:text-4xl mt-5'>Flight school lessons all over Tampa Bay</h2>
 
                     <HashLink to="/flight-school#who-we-are-section">
-                        <Button className='uppercase bg-primary text-white rounded-none mt-10'>Begin tour</Button>
+                        <Button size='lg' className='uppercase bg-primary text-white rounded-none mt-10'>Begin tour</Button>
                     </HashLink>
                 </div>
             </div>
