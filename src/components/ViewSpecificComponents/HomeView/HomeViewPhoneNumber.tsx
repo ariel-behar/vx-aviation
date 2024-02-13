8f075a65ce077908386e4f20b105d32e92f84232
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 15:45:04 +0200

    Creating the PhoneIcon component and implementing it into the HomeViewPhoneNumber component

---
+++ src/components/ViewSpecificComponents/HomeView/HomeViewPhoneNumber.tsx
@@ -0,0 +1,13 @@
+import PhoneIcon from '../../Icons/PhoneIcon'
+
+function HomeViewPhoneNumber() {
+	return (
+		<div className='mt-10 mx-auto flex flex-row justify-center items-center pulse'>
+			<PhoneIcon className="h-12 w-12" fillColor='#2E7D32' strokeColor='#2E7D32' strokeWidth={0.2} />
+
+			<p className='text-3xl text-white'>+1 (111) 222-3334</p>
+		</div>
+	)
+}
+
+export default HomeViewPhoneNumber
