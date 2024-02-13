154b9939bc435113e65afb3d9e1f45b40d959d56
Ariel Behar <ariel.behar@hotmail.com>
2024-02-13 21:39:45 +0200

    Installing font awesome and adding an icons' library in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { Suspense } from 'react'
 import { RouterProvider } from 'react-router-dom'
 import { ThemeProvider } from '@material-tailwind/react/context/theme'
+
+import { faPlane, faPhone } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core'
 
 import './App.scss'
 
 import router from './router/routes'
-import { Suspense } from 'react'
+
 import Loader from './components/Loader'
+
+library.add(faPlane, faPhone)
 
 function App() {
 
