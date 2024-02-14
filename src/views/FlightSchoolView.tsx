819e69cd80c9e93a33fdeb9148d148ac65cbcec5
Ariel Behar <ariel.behar@hotmail.com>
2024-02-14 15:42:38 +0200

    Creating the WhyChooseUsSection and adding it to the FlightSchoolView

--- src/views/FlightSchoolView.tsx
+++ src/views/FlightSchoolView.tsx
@@ -3,6 +3,7 @@ import HeroSection from "../components/ViewSpecificComponents/FlightSchoolView/H
 import ImagesSection from "../components/ViewSpecificComponents/FlightSchoolView/ImagesSection"
 import UpToDateTrainingSection from "../components/ViewSpecificComponents/FlightSchoolView/UpToDateTrainingSection"
 import WhoWeAreSection from "../components/ViewSpecificComponents/FlightSchoolView/WhoWeAreSection"
+import WhyChooseUsSection from "../components/ViewSpecificComponents/FlightSchoolView/WhyChooseUsSection"
 
 function FlightSchoolView() {
 	return (
@@ -11,8 +12,14 @@ function FlightSchoolView() {
 			<ImagesSection />
 			<WhoWeAreSection />
 			<UpToDateTrainingSection />
-			<CoursesSection />
-			
+			<div className="bg-custom-gradient border-b-1 border-b-primary">
+				<CoursesSection />
+
+				<hr className="border-primary border-1 w-1/4 mx-auto" />
+
+				<WhyChooseUsSection />
+			</div>
+
 		</>
 	)
 }
