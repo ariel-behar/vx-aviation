df2cea5280bbdbb0b1bcc47f252a0c520020894b
Ariel Behar <ariel.behar@hotmail.com>
2024-02-14 15:39:23 +0200

    Adjusting font sizes

--- src/components/ViewSpecificComponents/FlightSchoolView/WhoWeAreSection.tsx
+++ src/components/ViewSpecificComponents/FlightSchoolView/WhoWeAreSection.tsx
@@ -21,12 +21,12 @@ function WhoWeAreSection() {
                 <div className='lg:w-3/5 text-white px-5 lg:px-10'>
                     <h3 className='text-center mb-10'>WHO WE ARE</h3>
 
-                    <p className='text-justify'>We are what you would consider a “one-stop-shop” for all your aviation needs. Our owner is a licensed flight instructor with over 25 years of experience who got his experience by mowing lawns at 14 years old and paying his way forward. Please see the list of ratings currently held.</p>
+                    <p className='text-justify text-xl'>We are what you would consider a “one-stop-shop” for all your aviation needs. Our owner is a licensed flight instructor with over 25 years of experience who got his experience by mowing lawns at 14 years old and paying his way forward. Please see the list of ratings currently held.</p>
 
                     <ul className='my-10 pl-5 lg:pl-8'>
                         {
                             ratingsList.map(rating => (
-                                <li key={uniqid()} className='my-3'>
+                                <li key={uniqid()} className='my-3 text-xl'>
                                     <FontAwesomeIcon icon="plane" color="#2E7D32" size="xl" className='-rotate-45 mr-3' />
                                     {rating}
                                 </li>
@@ -34,7 +34,7 @@ function WhoWeAreSection() {
                         }
                     </ul>
 
-                    <p className='text-center text-2xl'>We welcome you to call anytime and look forward to meeting you</p>
+                    <p className='text-center text-xl'>We welcome you to call anytime and look forward to meeting you</p>
                 </div>
             </div>
 
