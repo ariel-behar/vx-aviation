340d7601056930ff3f04458dcfddb87b9bc9b2e8
Ariel Behar <ariel.behar@hotmail.com>
2024-02-14 15:59:11 +0200

    Creating a new phone.ts file and importing the number

--- src/components/ViewSpecificComponents/HomeView/HomeViewPhoneNumber.tsx
+++ src/components/ViewSpecificComponents/HomeView/HomeViewPhoneNumber.tsx
@@ -1,10 +1,12 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+import phoneNumber from '../../../data/phone'
+
 function HomeViewPhoneNumber() {
 	return (
 		<div className='mt-10 mx-auto flex flex-row justify-center items-center custom-pulse-animation'>
 			<FontAwesomeIcon icon="phone" color="#2E7D32" size="3x" className='mr-2'/>
-			<p className='text-3xl text-white'>+1 (111) 222-3334</p>
+			<p className='text-3xl text-white'>{phoneNumber}</p>
 		</div>
 	)
 }
---
+++ src/data/phone.ts
@@ -0,0 +1,3 @@
+const phoneNumber: string = "+1 (111) 222-3334";
+
+export default phoneNumber;
