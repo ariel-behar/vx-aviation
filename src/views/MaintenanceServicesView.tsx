7e8a0393eb8f9da131325b3419c53bd8fb89e7e5
Ariel Behar <ariel.behar@hotmail.com>
2024-02-14 22:33:51 +0200

    Creating the first title section of the MaintenanceServicesView

--- src/views/MaintenanceServicesView.tsx
+++ src/views/MaintenanceServicesView.tsx
@@ -1,8 +1,13 @@
+import airplaneRepair from '../assets/img/airplane-repair.jpg'
 
 function MaintenanceServicesView() {
-  return (
-    <div>MaintainanceServicesView</div>
-  )
+	return (
+		<div className="mt-[10vh] h-[55vh] bg-no-repeat bg-cover bg-fixed relative" style={{ backgroundImage: `url(${airplaneRepair})` }}>
+			<div className='absolute top-0 left-0 w-full h-full bg-black bg-opacity-70 flex justify-center items-center'>
+				<h2 className='uppercase text-white text-5xl'>Maintenance and Services</h2>
+			</div>
+		</div>
+	)
 }
 
 export default MaintenanceServicesView
