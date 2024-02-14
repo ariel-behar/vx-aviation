913f0eb274482247365e82435b29f6c46c3430a5
Ariel Behar <ariel.behar@hotmail.com>
2024-02-14 11:05:43 +0200

    Creating the getImageUrl util function

---
+++ src/utils/getImageUrl.ts
@@ -0,0 +1,8 @@
+function getImageUrl(imageFolder: string | null | undefined, imageName: string){
+    if(!imageFolder){
+        return new URL(`../assets/img/${imageName}`, import.meta.url).href;
+    }
+    return new URL(`../assets/img/${imageFolder}/${imageName}`, import.meta.url).href;
+}
+
+export default getImageUrl;
