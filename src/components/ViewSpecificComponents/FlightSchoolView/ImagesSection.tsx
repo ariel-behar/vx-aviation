a9dc29d22aca7cfc557221e56dfc9db724bbf70d
Ariel Behar <ariel.behar@hotmail.com>
2024-02-14 09:45:39 +0200

    Adding a missing key prop

--- src/components/ViewSpecificComponents/FlightSchoolView/ImagesSection.tsx
+++ src/components/ViewSpecificComponents/FlightSchoolView/ImagesSection.tsx
@@ -1,3 +1,4 @@
+import uniqid from 'uniqid'
 import airplaneFlying from '../../../assets/img/airplane-flying.jpg'
 import cockpit from '../../../assets/img/cockpit.jpg'
 import pilotEpaulettes from '../../../assets/img/pilot-epaulettes.jpg'
@@ -27,7 +28,7 @@ function ImagesSection() {
         <section className="grid grid-cols-3">
             {
                 images.map(({ imgSrc, alt }: Image) => (
-                    <div className='relative'>
+                    <div className='relative' key={uniqid()}>
                         <img src={imgSrc} alt={alt} />
                         <div className='absolute top-0 left-0 w-full h-full bg-black/30' />
                     </div>
