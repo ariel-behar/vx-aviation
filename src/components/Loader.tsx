09403a0b5232a9c88317f80edc01be4cde91946f
Ariel Behar <ariel.behar@hotmail.com>
2024-02-14 21:27:48 +0200

    Changing the background and making the loader's size bigger

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,9 +1,9 @@
 
 function Loader() {
     return (
-        <div className="w-screen h-screen bg-tertiary flex justify-center items-center z-50">
+        <div className="w-screen h-screen bg-custom-gradient flex justify-center items-center z-50">
             <p>
-                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="144" height="48" viewBox="0 0 144 48">
+                <svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="288" height="96" viewBox="0 0 144 48">
                     <path className="origin-bottom custom-take-off-animation" fill="#5B9144" d="M59.124,34L56,29h-4l2.947,11H89c1.657,0,3-1.343,3-3s-1.343-3-3-3H78.998L69,18h-4l4.287,16H59.124z" />
                     <rect id="ground" x="52" y="44" fill="#004080" width="40" height="4" />
                 </svg>
