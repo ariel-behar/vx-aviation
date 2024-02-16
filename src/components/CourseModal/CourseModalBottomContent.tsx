dd93fa1225be048680d9a764fc737660d8f325be
Ariel Behar <ariel.behar@hotmail.com>
2024-02-16 10:42:54 +0200

    Finalizing all of the custom styles for all possible course structures

--- src/components/CourseModal/CourseModalBottomContent.tsx
+++ src/components/CourseModal/CourseModalBottomContent.tsx
@@ -1,30 +1,34 @@
 import uniqid from 'uniqid'
+
 import getImageUrl from '../../utils/getImageUrl'
+
 import { ICourse } from '../ViewSpecificComponents/FlightSchoolView/CoursesSection'
 
-interface Props extends ICourse {}
+interface Props extends ICourse { }
 
-function CourseModalBottomContent({...course}: Props) {
+function CourseModalBottomContent({ ...course }: Props) {
     return (
         <>
             <hr className="mx-auto my-3 w-3/4 border-secondary" />
 
-            <div className="grid grid-cols-12 py-3 gap-3">
+            <div className="grid grid-cols-12 py-3 gap-4 px-2">
                 {
                     course.bookImage && (
-                        <div className="px-3 col-span-6">
-                            <img key={uniqid()} src={getImageUrl(`courses/${course.imagesFolder}`, course.bookImage)} alt={course.bookImage} loading="lazy" className="w-auto h-full" />
+                        <div className="px-3 col-span-5">
+                            <img key={uniqid()} src={getImageUrl(`courses/${course.imagesFolder}`, course.bookImage)} alt={course.bookImage} loading="lazy" className="w-auto h-full mx-auto" />
                         </div>
                     )
                 }
 
                 {
                     course.requirements && (
-                        <div className={`px-3 ${!course.bookImage ? "col-span-12 flex justify-center" : "col-span-6"}`}>
+                        <div className={`px-3 ${!course.bookImage ? "col-span-12 flex justify-center" : "col-span-7"}`}>
                             <ul className="list-disc">
                                 {
-                                    course.requirements.map((requirement:string) => (
-                                        <li key={uniqid()}>{requirement}</li>
+                                    course.requirements.map((requirement: string) => (
+                                        <li key={uniqid()}>
+                                            <p className="text-justify text-sm">{requirement}</p>
+                                        </li>
                                     ))
                                 }
                             </ul>
--- src/components/CourseModal/CourseModalTopContent.tsx
+++ src/components/CourseModal/CourseModalTopContent.tsx
@@ -1,57 +1,63 @@
 import { useState } from "react";
-import getImageUrl from "../../utils/getImageUrl"
-import { ICourse } from "../ViewSpecificComponents/FlightSchoolView/CoursesSection"
 import uniqid from "uniqid";
 
-interface Props extends ICourse {}
+import getImageUrl from "../../utils/getImageUrl"
+
+import { ICourse } from "../ViewSpecificComponents/FlightSchoolView/CoursesSection"
+
+interface Props extends ICourse { }
 
 function CourseModalTopContent({
     ...course
 }: Props) {
-	const [activeImage, setActiveImage] = useState<string>(course.courseImages[1])
+    const [activeImage, setActiveImage] = useState<string>(course.courseImages[1])
 
-	const changeActiveImage = (image: string) => {
-		let currentImage = image;
-		setActiveImage(prevState => currentImage)
-	}
+    const changeActiveImage = (image: string) => {
+        let currentImage = image;
+        setActiveImage(prevState => currentImage)
+    }
 
     return (
-        <div className="grid grid-cols-12 py-3 gap-3">
-            <div className={` ${course.courseImages.length > 2 ? "col-span-6" : "col-span-12"}`}>
+        <div className={`${(course.courseImages.length === 2 ) ? "" : "grid grid-cols-12 gap-4"} py-3 px-2`}>
+            <div className={` ${course.courseImages.length > 2 ? "col-span-5" : "col-span-12"}`}>
 
-                {
-                    course.courseImages.length > 1 && (
-                        <img src={getImageUrl(`courses/${course.imagesFolder}`, activeImage)} alt={activeImage} className=" w-3/5 mx-auto float-left" />
-                    )
-                }
+                {/* Image displayed when course has ONLY 1 image aside from the cover image */}
+                {course.courseImages.length == 2 && <img src={getImageUrl(`courses/${course.imagesFolder}`, activeImage)} alt={activeImage} className="w-3/5 float-left pr-5" />}
 
-                {
-                    course.courseImages.length > 2 && (
+                {/* Images displayed when course has MORE THAN 1 image aside from the cover image */}
+                {course.courseImages.length > 2 && (
+                    <>
+                        <img src={getImageUrl(`courses/${course.imagesFolder}`, activeImage)} alt={activeImage} className="w-full mt-3" />
                         <div className="grid grid-cols-3 mt-2 gap-2">
                             {
                                 course.courseImages.map((image, index) => {
                                     if (index != 0) {
-                                        return <img key={uniqid()} src={getImageUrl(`courses/${course.imagesFolder}`, image)} alt={image} className="w-full" onClick={() => changeActiveImage(image)} />
+                                        return <img key={uniqid()} src={getImageUrl(`courses/${course.imagesFolder}`, image)} alt={image} className="w-full cursor-pointer hover:shadow-lg hover:scale-110 transition-all duration-300" onClick={() => changeActiveImage(image)} />
                                     }
                                 })}
                         </div>
-                    )
-                }
+                    </>
+                )}
+
             </div>
+
             {
-                (course.courseImages.length > 1 && course.overview.length > 1)
+                
+                (course.courseImages.length === 2 )
                     ? (
-                        <div className={` clear-left `}>
+                        <>
+                            {/* Content display for courses with ONLY 1 image aside from the cover */}
                             <h5 className="text-center">Overview</h5>
                             {course.overview.map((paragraph) => {
-                                return <p key={uniqid()}>{paragraph}</p>
+                                return <p key={uniqid()} className="text-justify text-sm mt-3">{paragraph}</p>
                             })}
-                        </div>
+                        </>
                     )
                     : (
-                        <div className={`${(course.bookImage || course.requirements) ? "col-span-6" : "col-span-12"} `}>
+                        // Content display for courses with MORE THAN 1 image aside from the cover 
+                        <div className={`${(course.bookImage || course.requirements) ? "col-span-7" : "col-span-12"} `}>
                             <h5 className="text-center">Overview</h5>
-                            {course.overview}
+                            <p className="text-justify text-sm">{course.overview}</p>
                         </div>
                     )
             }
