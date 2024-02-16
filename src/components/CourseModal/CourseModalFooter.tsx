6cbb9bcd68fdbe214676f353b0ba29d0fa3249a9
Ariel Behar <ariel.behar@hotmail.com>
2024-02-16 17:27:56 +0200

    Optimizing for mobile

--- src/components/CourseModal/CourseModalFooter.tsx
+++ src/components/CourseModal/CourseModalFooter.tsx
@@ -8,9 +8,9 @@ function CourseModalFooter() {
     const { handleShowModal } = useCourseModalContext();
 
     return (
-        <footer className="bg-custom-gradient p-5 border-t-2 border-primary flex items-center justify-center">
+        <footer className="bg-custom-gradient p-5 border-t-2 border-primary flex justify-center items-center ">
             <div className="text-white ml-auto">
-                <h5 className='uppercase text-center mb-5'>Call us for more details about this program</h5>
+                <h5 className='uppercase text-center text-sm md:text-lg mb-2'>Call us for more details about this program</h5>
                 <PhoneNumber />
             </div>
             <Button className="bg-secondary rounded-md ml-auto" onClick={handleShowModal}>Close</Button>
