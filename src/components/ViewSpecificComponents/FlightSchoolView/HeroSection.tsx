f90decf39f0c91677283a78568391435b8cb10d3
Ariel Behar <ariel.behar@hotmail.com>
2024-02-16 10:47:05 +0200

    Creating an animation for the HeroSection's titles

--- src/components/ViewSpecificComponents/FlightSchoolView/HeroSection.tsx
+++ src/components/ViewSpecificComponents/FlightSchoolView/HeroSection.tsx
@@ -1,4 +1,5 @@
 import { Link as ScrollLink } from 'react-scroll'
+import { motion } from 'framer-motion'
 
 import pilotGear from '../../../assets/img/pilot-gear.jpg'
 import pilot from '../../../assets/img/pilot.png'
@@ -7,16 +8,21 @@ import Button from '@material-tailwind/react/components/Button'
 
 function HeroSection() {
     return (
-        <section className='relative h-screen bg-no-repeat bg-cover bg-top lg:bg-fixed' style={{backgroundImage: `url(${pilotGear})`}}>
+        <section className='relative h-screen bg-no-repeat bg-cover bg-top lg:bg-fixed' style={{ backgroundImage: `url(${pilotGear})` }}>
             <div className='bg-black/60 absolute  top-0 left-0 h-screen w-full z-[1] flex justify-center items-center'>
-                <div className='text-center'>
-                    <h1 >WELCOME TO V<sub>x</sub> AVIATION</h1>
+                <motion.div
+                    initial={{ opacity: 0, y: -100 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ duration: 1 }}
+                    className='text-center'
+                >
+                    <h1>WELCOME TO V<sub>x</sub> AVIATION</h1>
                     <h2 className='mt-5'>Flight school lessons all over Tampa Bay</h2>
 
                     <ScrollLink className='cursor-pointer' to="who-we-are-section" smooth={true} offset={-100} duration={500}>
                         <Button size='lg' className='uppercase bg-primary rounded-md mt-10 font-thin'>Begin tour</Button>
                     </ScrollLink>
-                </div>
+                </motion.div>
             </div>
 
             <img src={pilot} alt='Pilot' className='absolute bottom-0 right-20 z-[3] hidden lg:block' />
