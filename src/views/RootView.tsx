797f18b14546a023973e64f5795ead08bc9dd505
Ariel Behar <ariel.behar@hotmail.com>
2024-02-16 09:35:58 +0200

    Conditionally displaying the CourseModal in the RootView

--- src/views/RootView.tsx
+++ src/views/RootView.tsx
@@ -1,9 +1,13 @@
 import { Outlet, ScrollRestoration, useLocation } from "react-router-dom";
 import { Header } from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import { useCourseModalContext } from "../context/CourseModalContext";
+import { lazy } from "react";
+const LazyCourseModal = lazy(() => import("../components/CourseModal"));
 
 function RootView() {
 	let location = useLocation();
+	const { selectedCourse, showModal, handleShowModal } = useCourseModalContext();
 
 	return (
 		<>
@@ -14,6 +18,8 @@ function RootView() {
 			<Outlet />
 
 			<Footer />
+
+			{(selectedCourse && showModal) && <LazyCourseModal {...selectedCourse} showModal={showModal} handleShowModal={handleShowModal} />}
 		</>
 	)
 }
