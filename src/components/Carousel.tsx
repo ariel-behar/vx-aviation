d434c24ae0a9c9a172e73a77a1366e79d106954f
Ariel Behar <ariel.behar@hotmail.com>
2024-02-16 17:21:11 +0200

    Creating the slides for each screen size

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -6,6 +6,7 @@ import CourseCard from "./CourseCard"
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import MuiCarousel from "@material-tailwind/react/components/Carousel";
+import useScreenSize from "../hooks/useScreenSize";
 
 interface Props {
     data: ICourse[]
@@ -14,6 +15,7 @@ interface Props {
 function CustomCarousel({
     data
 }: Props) {
+    const screenSize = useScreenSize();
 
     const chevronStyles = "text-white text-lg bg-primary rounded-full py-3 px-4 hover:scale-110 cursor-pointer transition-all ease-in-out hover:bg-green-700 active:bg-green-900"
 
@@ -41,20 +43,49 @@ function CustomCarousel({
             )}
             transition={{ duration: 0.3 }} >
 
-            {/* Slide 1 */}
-            <div className="gap-5 grid grid-cols-3  px-5">
-                {
-                    data.map((course, index) => (index <= 2) && <CourseCard {...course} key={uniqid()} />)
-                }
-            </div>
+            {/* The below code is done this way, because the Carousel component is looking for the children div elements to create the slides. For this reason, each potential slide is separated in this strange way. If the slides are exported to a different component (ex. CarouselSlides), it will not work as expected, because Carousel will consider the fragment as a child and the slides will not be displayed properly. */}
 
-            {/* Slide 2 */}
-            <div className="flex gap-5  px-5">
-                {
-                    data.map((course, index) => (index > 2) && <CourseCard {...course} key={uniqid()} />)
-                }
-            </div>
-        </MuiCarousel>
+            {/* On "xs" display 6 slides with 1 card*/}
+            {(screenSize === "xs") && (
+                data.map(course => (
+                    <div key={uniqid()} className="px-5">
+                        <CourseCard {...course} />
+                    </div>
+                ))
+            )}
+
+            {/* On "sm" and "md" display 3 slides with 2 cards*/}
+            {(screenSize === "sm" || screenSize === "md") && (
+                <div className="px-5 grid grid-cols-2 gap-3">
+                    {data.map((course, index) => (index <= 1) && <CourseCard {...course} key={uniqid()} />)}
+                </div>
+            )}
+
+            {(screenSize === "sm" || screenSize === "md") && (
+                <div className="px-5 grid grid-cols-2 gap-3">
+                    {data.map((course, index) => (index > 1 && index <= 3) && <CourseCard {...course} key={uniqid()} />)}
+                </div>
+            )}
+
+            {(screenSize === "sm" || screenSize === "md") && (
+                <div className="px-5 grid grid-cols-2 gap-3">
+                    {data.map((course, index) => (index > 3 && index <= 5) && <CourseCard {...course} key={uniqid()} />)}
+                </div>
+            )}
+
+            {/* On "lg", "xl" and "2xl" display 2 slides with 3 cards*/}
+            {(screenSize === "lg" || screenSize === "xl" || screenSize === "2xl") && (
+                <div className="px-5 grid grid-cols-3 gap-3">
+                    {data.map((course, index) => (index <= 2) && <CourseCard {...course} key={uniqid()} />)}
+                </div>
+            )}
+
+            {(screenSize === "lg" || screenSize === "xl" || screenSize === "2xl") && (
+                <div className="px-5 grid grid-cols-3 gap-3">
+                    {data.map((course, index) => (index > 2) && <CourseCard {...course} key={uniqid()} />)}
+                </div>
+            )}
+        </MuiCarousel >
     )
 }
 
