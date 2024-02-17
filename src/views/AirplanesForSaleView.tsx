f08a4e109205ba270fd128253f036fab3bffd9b8
Ariel Behar <ariel.behar@hotmail.com>
2024-02-17 18:51:56 +0200

    Implementing HelmetSEO on the AirplanesForSaleView

--- src/views/AirplanesForSaleView.tsx
+++ src/views/AirplanesForSaleView.tsx
@@ -3,25 +3,31 @@ import { motion } from 'framer-motion'
 import pattern from '../assets/img/pattern/mooning.jpg'
 import airplaneBanner from '../assets/img/airplane-banner.png'
 import vxAviationLogo from '../assets/img/logos/vx-aviation-logo-with-title-effects.png'
+import HelmetSEO from '../components/HelmetSEO'
 
 function AirplanesForSaleView() {
 	return (
-		<section className="mt-[10vh] flex flex-col justify-center items-center pt-5 pb-10 md:py-20 px-5" style={{ backgroundImage: `url(${pattern})` }}>
-			<h3 className='uppercase text-2xl sm:text-4xl text-center text-tertiary mt-10 mb-16 font-medium'>No active listings are currently available</h3>
-			<div className='overflow-hidden'>
-				<motion.img
-					src={airplaneBanner}
-					alt="Airplane Banner"
-					initial={{ x: "100%" }}
-					animate={{ x: 0}}
-					transition={{ duration: 1, ease: "easeInOut" }}
-				/>
-			</div>
-			<div className='flex items-center mt-12'>
-				<p className='text-tertiary'>Sincerely,</p>
-				<img src={vxAviationLogo} alt='Vx Aviation Logo' className='ml-5 max-h-[100px] lg:max-h-40' />
-			</div>
-		</section>
+		<>
+			<HelmetSEO title='AIRPLANES FOR SALE' description='Find Your Wings: Discover our selection of quality airplanes for sale at VX Aviation. From single-engine to commercial aircraft, explore your options and take flight with confidence.' />
+
+
+			<section className="mt-[10vh] flex flex-col justify-center items-center pt-5 pb-10 md:py-20 px-5" style={{ backgroundImage: `url(${pattern})` }}>
+				<h3 className='uppercase text-2xl sm:text-4xl text-center text-tertiary mt-10 mb-16 font-medium'>No active listings are currently available</h3>
+				<div className='overflow-hidden'>
+					<motion.img
+						src={airplaneBanner}
+						alt="Airplane Banner"
+						initial={{ x: "100%" }}
+						animate={{ x: 0 }}
+						transition={{ duration: 1, ease: "easeInOut" }}
+					/>
+				</div>
+				<div className='flex items-center mt-12'>
+					<p className='text-tertiary'>Sincerely,</p>
+					<img src={vxAviationLogo} alt='Vx Aviation Logo' className='ml-5 max-h-[100px] lg:max-h-40' />
+				</div>
+			</section>
+		</>
 	)
 }
 
