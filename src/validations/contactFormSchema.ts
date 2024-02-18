1e443932a40cb0101a7d7798e6e3d0d811466a88
Ariel Behar <ariel.behar@hotmail.com>
2024-02-18 13:50:03 +0200

    Creating a very basic validation schema only for email and phone number

--- src/validations/contactFormSchema.ts
+++ src/validations/contactFormSchema.ts
@@ -2,20 +2,14 @@ import * as yup from 'yup';
 
 const contactFormShape = {
     name: yup.string()
+        .min(2, 'Name must be at least 2 characters')
+        .max(30, 'Name must be at most 30 characters')
         .required('Name is required'),
     phone: yup.string()
         .matches(/^[0-9]+$/, 'Phone number must contain only digits')
         .min(6, 'Phone number must be at least 6 characters')
         .max(12, 'Phone number must be at most 12 characters')
-        .required('Phone number is required'),
-    courses: yup.array()
-        .of(yup.string())
-        .min(1, 'Please select at least one program')
-        .required('Please select at least one program'),
-    contactHours: yup.array()
-        .of(yup.string())
-        .min(1, 'Please select at least one contact hour time frame ')
-        .required('Please select at least one contact hour time frame'),
+        .required('Phone number is required')
 }
 
 const contactFormSchema = yup.object().shape(contactFormShape)
