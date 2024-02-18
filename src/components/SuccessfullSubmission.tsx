3fe66551e32b875442be69c3efc3c1987f9e80ad
Ariel Behar <ariel.behar@hotmail.com>
2024-02-18 13:51:09 +0200

    Creating the SuccessfullSubmission component

---
+++ src/components/SuccessfullSubmission.tsx
@@ -0,0 +1,11 @@
+function SuccessfullSubmission() {
+  return (
+    <div className='h-full flex flex-col items-center justify-center'>
+        <h3 className="text-green-500 text-center mb-3">Thank you for your submission!</h3>
+			<p className="text-center fw-bold">(Please note that the website is no longer maintained and no actual form submission has been made)</p>
+
+    </div>
+  )
+}
+
+export default SuccessfullSubmission
